0e84e1785dea0845d32349f150cefc2e39621167
Martin Vielsmaier <martin@vielsmaier.net>
2023-10-20 10:33:57 +0200

    Init

---
+++ README.md
@@ -0,0 +1,10 @@
+# Depeche DB
+## Example: Bank
+
+[![Tests](https://github.com/depeche-py/example-bank/actions/workflows/tests.yml/badge.svg)](https://github.com/depeche-py/example-bank/actions/workflows/tests.yml)
+[![license](https://img.shields.io/github/license/depeche-py/example-bank.svg)](https://github.com/depeche-py/example-bank/blob/main/LICENSE)
+
+---
+
+An example of using [depeche-db](https://github.com/depeche-py/depeche-db) to
+build an event-sourced application.
---
+++ tests/test_mypy.py
@@ -0,0 +1,28 @@
+import pathlib as _pathlib
+import subprocess as _subprocess
+import sys as _sys
+
+
+def test_typing():
+    ini = "mypy.ini"
+    if _sys.version_info < (3, 10):
+        ini = "mypy39.ini"
+    repo_path = _pathlib.Path(__file__).parent.parent
+    res = _subprocess.run(
+        [
+            "poetry",
+            "run",
+            "--",
+            "mypy",
+            "--config",
+            str(repo_path / ini),
+            ".",
+            "--show-traceback",
+        ],
+        capture_output=True,
+    )
+    if res.returncode != 0:
+        print(f"`{' '.join(res.args)}` exited with error: {res.returncode}")
+        print(res.stdout.decode())
+        print(res.stderr.decode())
+        raise AssertionError("Mypy failed")
